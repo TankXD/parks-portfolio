1b606752d7b335dd838997ab3c83d48ef657d5af
TankXD <tjwcskdle1@gmail.com>
2023-03-23 02:40:46 +0900

    FIX : Work category selected

--- js/work.js
+++ js/work.js
@@ -2,21 +2,31 @@ const categories = document.querySelector(".work__categories");
 const projectContainer = document.querySelector(".work__projects");
 const projects = document.querySelectorAll(".project");
 
-// 예전 타겟 저장해두고 새 타겟이 눌리면 예전 타겟의 active클래스 삭제해주기 위한
+// 예전 타겟 저장해두고 새 타겟이 눌리면 예전 타겟의 selected클래스 삭제해주기 위한
 // 변수 미리 생성
 let preTarget;
+
+const handleSelected = (e) => {
+  // 순서가 중요
+  // 2. 다시 버튼 클릭하는 경우 이전 타겟의 active 클래스 삭제.
+  if (preTarget) {
+    preTarget.classList.remove("selected");
+  }
+  if (e.target.tagName === "SPAN") {
+    const targetBtn = e.target.parentNode;
+    targetBtn.classList.add("selected");
+    preTarget = targetBtn;
+  } else {
+    e.target.classList.add("selected");
+    preTarget = e.target;
+  }
+  // 1. 이전 타겟을 나중에 저장해주고
+};
 
 const filterProject = (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
 
-  // 순서가 중요
-  // 2. 다시 버튼 클릭하는 경우 이전 타겟의 active 클래스 삭제.
-  if (preTarget) {
-    preTarget.classList.remove("active");
-  }
-  e.target.classList.add("active");
-  // 1. 이전 타겟을 나중에 저장해주고
-  preTarget = e.target;
+  handleSelected(e);
 
   projectContainer.classList.add("anima");
   // anima클래스가 계속 적용되어있으면 안되서 3초뒤에 삭제해줌.
